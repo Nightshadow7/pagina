cea48c8e1f0dc8f8e30e4212352f0b72a0371b66
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-04-21 02:49:18 -0500

    :roket:final final finalisimo menos pocas imagenes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
-  window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('show', window.scrollY > 20);
-  });
+  // window.addEventListener('scroll', function() {
+  //   const navbar = document.querySelector('.navbar');
+  //   navbar.classList.toggle('show', window.scrollY > 20);
+  // });
+
+  $('.botonF1').hover(function(){
+    $('.btn').addClass('animacionVer');
+  })
+  $('.contenedor').mouseleave(function(){
+    $('.btn').removeClass('animacionVer');
+  }) 
 
